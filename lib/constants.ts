df4a17b42f1dfa2a893654652cce91b1c3c2a843
Matt Duff <matt.mpdee@gmail.com>
2025-12-23 20:34:31 +0000

    fix(maintenance): create missing constants file
    - Created lib/constants.ts with DVLA_EXCLUDED_REG_NUMBERS
    - Resolves build error: Module not found '@/lib/constants'
    - Exports test vehicle registration numbers to exclude from sync

---
+++ lib/constants.ts
@@ -0,0 +1,8 @@
+// Application-wide constants
+
+/**
+ * Registration numbers to exclude from DVLA sync
+ * These are test vehicles with non-real VRNs
+ */
+export const DVLA_EXCLUDED_REG_NUMBERS = ['TE57 VAN', 'TE57 HGV'];
+
